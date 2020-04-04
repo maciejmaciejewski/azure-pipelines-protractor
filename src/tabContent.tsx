3dec0153901173f4c4ef0b8c9c094a5cd0a49159
Alexandre Feblot <alexandre.feblot@gmail.com>
2020-04-04 22:47:29 +0200

    handle error image

--- src/tabContent.tsx
+++ src/tabContent.tsx
@@ -23,58 +23,66 @@ const OUR_TASK_IDS = ["f921c333-68d3-50ff-9d01-c71ecafad96b", "3b603ffa-eaed-5a3
 
 SDK.init()
 SDK.ready().then(() => {
-    try {
-        const config = SDK.getConfiguration()
-        if (typeof config.onBuildChanged === "function") {
-            config.onBuildChanged((build: Build) => {
-                let buildAttachmentClient = new BuildAttachmentClient(build)
-                buildAttachmentClient.init().then(() => {
-                    displayReports(buildAttachmentClient)
-                }).catch(error => {setError(error)})
-            })
-        } else if (typeof config.releaseEnvironment === "object") {
-            let releaseAttachmentClient = new ReleaseAttachmentClient(config.releaseEnvironment)
-            releaseAttachmentClient.init().then(() => {
-                displayReports(releaseAttachmentClient)
-            }).catch(error => {setError(error)})
-        }
-    } catch(error) {
-        setError(error)
+  try {
+    const config = SDK.getConfiguration()
+    if (typeof config.onBuildChanged === "function") {
+      config.onBuildChanged((build: Build) => {
+        let buildAttachmentClient = new BuildAttachmentClient(build)
+        buildAttachmentClient.init().then(() => {
+          displayReports(buildAttachmentClient)
+        }).catch(error => {setError(error)})
+      })
+    } else if (typeof config.releaseEnvironment === "object") {
+      let releaseAttachmentClient = new ReleaseAttachmentClient(config.releaseEnvironment)
+      releaseAttachmentClient.init().then(() => {
+        displayReports(releaseAttachmentClient)
+      }).catch(error => {setError(error)})
     }
+  } catch(error) {
+    setError(error)
+  }
 })
 
 function setText (message: string) {
-    console.log(message)
-    const messageContainer = document.querySelector("#protractor-ext-message p")
-    if (messageContainer) {
-        messageContainer.innerHTML = message
-    }
+  console.log(message)
+  const messageContainer = document.querySelector("#protractor-ext-message p")
+  if (messageContainer) {
+    messageContainer.innerHTML = message
+  }
+  const spinner = document.querySelector(".spinner")
+
 }
 
 function setError (error: Error) {
-    setText('Error loading reports')
-    console.log(error)
+  setText('Error loading reports')
+  console.log(error)
+  const spinner = document.querySelector(".spinner") as HTMLElement;
+  const errorBadge = document.querySelector('.error-badge') as HTMLElement;
+  if (spinner && errorBadge) {
+    spinner.style.display = 'none';
+    errorBadge.style.display = 'block';
+  }
 }
 
 function displayReports(attachmentClient: AttachmentClient) {
-    const nbAttachments = attachmentClient.getAttachments().length
-    if (nbAttachments) {
-        ReactDOM.render(<TaskAttachmentPanel attachmentClient={attachmentClient} />, document.getElementById("protractor-ext-container"))
-        document.getElementById("protractor-ext-message").style.display = "none"
-    } else {
-        setText("Can't find any report attachment")
-    }
+  const nbAttachments = attachmentClient.getAttachments().length
+  if (nbAttachments) {
+    ReactDOM.render(<TaskAttachmentPanel attachmentClient={attachmentClient} />, document.getElementById("protractor-ext-container"))
+    document.getElementById("protractor-ext-message").style.display = "none"
+  } else {
+  setError("Can't find any report attachment")
+  }
 }
 
 SDK.register("registerRelease", {
   isInvisible: function (state) {
-      let resultArray = []
-      state.releaseEnvironment.deployPhasesSnapshot.forEach(phase => {
-          phase.workflowTasks.forEach(task => {
-              resultArray.push(task.taskId)
-          })
+    let resultArray = []
+    state.releaseEnvironment.deployPhasesSnapshot.forEach(phase => {
+      phase.workflowTasks.forEach(task => {
+        resultArray.push(task.taskId)
       })
-      return !OUR_TASK_IDS.some(id => resultArray.includes(id))
+    })
+    return !OUR_TASK_IDS.some(id => resultArray.includes(id))
   }
 })
 
@@ -111,47 +119,46 @@ export default class TaskAttachmentPanel extends React.Component<TaskAttachmentP
   }
 
   public render() {
-      const attachments = this.props.attachmentClient.getAttachments()
-      if (attachments.length == 0) {
-          return (null)
-      } else {
-          const tabs = []
-          for (const attachment of attachments) {
-              tabs.push(<Tab name={attachment.name} id={attachment.name} key={attachment.name} url={attachment._links.self.href}/>)
-              this.tabContents.add(attachment.name, this.tabInitialContent)
-          }
-          return (
-              <div className="flex-column">
-                  { attachments.length > 1 ?
-                      <TabBar
-                          onSelectedTabChanged={this.onSelectedTabChanged}
-                          selectedTabId={this.selectedTabId}
-                          tabSize={TabSize.Tall}
-                      >
-                          {tabs}
-
-                      </TabBar>
-                  : null }
-                  <Observer selectedTabId={this.selectedTabId} tabContents={this.tabContents}>
-                      {(props: { selectedTabId: string }) => {
-                          if ( this.tabContents.get(props.selectedTabId) === this.tabInitialContent) {
-                              this.props.attachmentClient.getAttachmentContent(props.selectedTabId).then((content) => {
-                                this.tabContents.set(props.selectedTabId, '<iframe class="wide" srcdoc="' + this.escapeHTML(content) + '"></iframe>')
-                              }).catch(error => {
-                                  this.tabContents.set(props.selectedTabId, '<div class="wide"><p>Error loading report:<br/>' + error + '</p></div>')
-                                  setError(error)
-                              })
-                          }
-                          return  <span dangerouslySetInnerHTML={ {__html: this.tabContents.get(props.selectedTabId)} } />
-                      }}
-                  </Observer>
-              </div>
-          );
+    const attachments = this.props.attachmentClient.getAttachments()
+    if (attachments.length == 0) {
+      return (null)
+    } else {
+      const tabs = []
+      for (const attachment of attachments) {
+        tabs.push(<Tab name={attachment.name} id={attachment.name} key={attachment.name} url={attachment._links.self.href}/>)
+        this.tabContents.add(attachment.name, this.tabInitialContent)
       }
+      return (
+        <div className="flex-column">
+          { attachments.length > 1 ?
+            <TabBar
+              onSelectedTabChanged={this.onSelectedTabChanged}
+              selectedTabId={this.selectedTabId}
+              tabSize={TabSize.Tall}
+            >
+              {tabs}
+            </TabBar>
+          : null }
+          <Observer selectedTabId={this.selectedTabId} tabContents={this.tabContents}>
+            {(props: { selectedTabId: string }) => {
+              if ( this.tabContents.get(props.selectedTabId) === this.tabInitialContent) {
+                this.props.attachmentClient.getAttachmentContent(props.selectedTabId).then((content) => {
+                  this.tabContents.set(props.selectedTabId, '<iframe class="wide" srcdoc="' + this.escapeHTML(content) + '"></iframe>')
+                }).catch(error => {
+                  this.tabContents.set(props.selectedTabId, '<div class="wide"><p>Error loading report:<br/>' + error + '</p></div>')
+                  setError(error)
+                })
+              }
+              return  <span dangerouslySetInnerHTML={ {__html: this.tabContents.get(props.selectedTabId)} } />
+            }}
+          </Observer>
+        </div>
+      );
+    }
   }
 
   private onSelectedTabChanged = (newTabId: string) => {
-      this.selectedTabId.value = newTabId;
+    this.selectedTabId.value = newTabId;
   }
 }
 
@@ -174,15 +181,15 @@ abstract class AttachmentClient {
   abstract async init(): Promise<void>
 
   public getAttachments() : Attachment[]  | ReleaseTaskAttachment[] {
-      return this.attachments
+    return this.attachments
   }
 
   public getDownloadableAttachment(attachmentName: string): Attachment | ReleaseTaskAttachment {
-      const attachment = this.attachments.find((attachment) => { return attachment.name === attachmentName})
-      if (!(attachment && attachment._links && attachment._links.self && attachment._links.self.href)) {
-          throw new Error("Attachment " + attachmentName + " is not downloadable")
-      }
-      return attachment
+    const attachment = this.attachments.find((attachment) => { return attachment.name === attachmentName})
+    if (!(attachment && attachment._links && attachment._links.self && attachment._links.self.href)) {
+      throw new Error("Attachment " + attachmentName + " is not downloadable")
+    }
+    return attachment
   }
 
   abstract async getScreenshotAttachments(): Promise<Attachment[] | ReleaseTaskAttachment[]>
@@ -190,16 +197,16 @@ abstract class AttachmentClient {
   public async getAttachmentContent(attachmentName: string): Promise<string> {
     setText('Looking for Report File')
     if (this.authHeaders === undefined) {
-        console.log('Get access token')
-        const accessToken = await SDK.getAccessToken()
-        const b64encodedAuth = Buffer.from(':' + accessToken).toString('base64')
-        this.authHeaders = { headers: {'Authorization': 'Basic ' + b64encodedAuth} }
+      console.log('Get access token')
+      const accessToken = await SDK.getAccessToken()
+      const b64encodedAuth = Buffer.from(':' + accessToken).toString('base64')
+      this.authHeaders = { headers: {'Authorization': 'Basic ' + b64encodedAuth} }
     }
     console.log("Get " + attachmentName + " attachment content")
     const attachment = this.getDownloadableAttachment(attachmentName)
     const response = await fetch(attachment._links.self.href, this.authHeaders)
     if (!response.ok) {
-        throw new Error(response.statusText)
+      throw new Error(response.statusText)
     }
     setText('Processing Report File')
     const contentJSON = JSON.parse(JSON.parse(await response.text()))
@@ -276,7 +283,7 @@ class ReleaseAttachmentClient extends AttachmentClient {
 
     const runPlanIds = deployStep.releaseDeployPhases.map((phase) => phase.runPlanId)
     if (!runPlanIds.length) {
-        throw new Error("There are no plan IDs");
+      throw new Error("There are no plan IDs");
     } else {
       searchForRunPlanId: {
         for (const phase of deployStep.releaseDeployPhases) {
@@ -296,10 +303,10 @@ class ReleaseAttachmentClient extends AttachmentClient {
     console.log('Get attachment list')
     this.attachments = await releaseClient.getReleaseTaskAttachments(project.id, releaseId, environmentId, deployStep.attempt, this.runPlanId, ATTACHMENT_TYPE)
     if (this.attachments.length === 0) {
-        throw new Error("There is no attachment")
+      throw new Error("There is no attachment")
     }
     if (this.attachments.length >1) {
-        throw new Error("There is more than a single attachment, this is not expected")
+      throw new Error("There is more than a single attachment, this is not expected")
     }
   }
 
